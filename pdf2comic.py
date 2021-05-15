8e5ec77b93ef0e242f14d16dd91477061faec8d3
SeansThings <SeansThings@users.noreply.github.com>
2021-05-15 19:16:47 +0100

    pdf2comic now uses command line arguments

--- pdf2comic.py
+++ pdf2comic.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python
+
 import os
+import sys
+import getopt
 import io
 import pdf2image
 import PIL
+# import pathlib
+from pathlib import Path
 from zipfile import ZipFile
 from alive_progress import alive_bar
-from time import sleep
-
-workingDir=os.environ['HOME'] + '/.pdf2comic'
-pdfDir=workingDir+'/pdf'
-imgDir=workingDir+'/img'
-archDir=workingDir+'/comics'
+import time
+import subprocess
 
 def trimBorder(im):
+    # Trim the borders on pages based on top left pixel colour
     bg = PIL.Image.new(im.mode, im.size, im.getpixel((0,0)))
     diff = PIL.ImageChops.difference(im, bg)
     diff = PIL.ImageChops.add(diff, diff, 1, -100)
@@ -20,49 +22,125 @@ def trimBorder(im):
     if bbox:
         return im.crop(bbox)
 
-# Check for working directory. If not found prompt for location.
-if os.path.isdir(workingDir)  == False:    
-    print('Working directory not found. Please enter location of working directory:')
-    workingDir = input('(e.g. /home/user/.pdf2comic): ')
-    os.mkdir(workingDir)
+def main(argv):
+    # Check the command line arguments
+    inputStr = 'pdf2comic.py -f <Comic format> -i <Input directory> -o <Output directory>'
+    try:
+        opts, args = getopt.getopt(argv,'hf:i:o:',['comicFormat=','inDir=','outDir='])
+    except getopt.GetoptError:
+        print(inputStr)
+        sys.exit(2)
+    
+    inDir = ''
+    outDir = ''
+    comicFormat = ''
 
-# Create PDF dir if missing
-if os.path.isdir(pdfDir) == False:
-    os.mkdir(pdfDir)
+    # Process command line arguments
+    if len(opts) >= 1:
+        for opt, arg in opts:
+            if opt == '-h':
+                print(inputStr)
+                sys.exit()
+            elif opt in ('-f', '--comicFormat'):
+                comicFormat = arg
+            elif opt in ('-i', '--inDir'):
+                inDir = Path(arg)
+            elif opt in ('-o', '--outDir'):
+                outDir = Path(arg)
+    else:
+        print('No arguments provided')
+        print(inputStr)
+        sys.exit()
+
+    # Determine comic format or default to cbz
+    if comicFormat.lower() in ('cbr', 'r'):
+        comicFormat = 'cbr'
+    elif comicFormat.lower() in ('cbz', 'z', ''):
+        comicFormat = 'cbz'
+    else:
+        print('Invalid comic format selected!')
+        print('Valid formats: cbr, z, cbr, r')
+
+    # Python 3.10 required
+    # match comicFormat.lower():
+    #     case 'cbz', 'z':
+    #       comicFormat = 'cbz'
+    #     case 'cbr':
+    #       comicFormat = 'cbr'
+    #     case _:
+    #        print('Invalid comic format selected!')
+    #        print('Valid formats: cbr, z, cbr, r')
+
+    # Check that the input directory exists and contains PDF files
+    # if os.path.isdir(inDir) == False:
+    if inDir.exists() == False:
+        print('Input directory does not exist!')
+        sys.exit()
+    elif sum(1 for _ in inDir.glob('*')) == 0:
+        print('Input directory is empty!')
+        sys.exit()
+    else:
+        for file in os.listdir(inDir):
+            if file.endswith('.pdf') == False and file.endswith('.PDF') == False:
+                print('No PDF files found in input directory!')
+                sys.exit()
+
+    # Create output dir if missing
+    if os.path.isdir(outDir) == False:
+        os.mkdir(outDir)
+
+    # Iterate through each PDF file found
+    for pdf in os.listdir(inDir):
+        baseFN=os.path.splitext(pdf)[0]
+        pageFN=baseFN.replace(' ', '_')
+
+        # Convert and store pages
+        # pages = pdf2image.convert_from_path(inDir + '/' + pdf)
+        pdfLocation = inDir / pdf
+        pages = pdf2image.convert_from_path(pdfLocation)
+
+        # Create zip file in ram
+        zipIO = io.BytesIO()
+        with ZipFile(zipIO, 'w') as comicZip:
+            
+            # Create progress bar
+            with alive_bar(len(pages), title=baseFN, title_length='40', length='40', bar='blocks', spinner='dots_reverse') as bar:
+
+                # Process each page
+                for i in range(len(pages)):
+                    # Trim pages
+                    pages[i] = trimBorder(pages[i])
+
+                    # Replace PIL images with BytesIO image
+                    pageIO = io.BytesIO()
+                    pages[i].save(pageIO, 'WEBP')
+                    pages[i].close()
+                    pages[i] = pageIO
+                    
+                    # Add pages to archive file
+                    if comicFormat == 'cbr':
+                        print('CBR\'s not yet supported!')
+                        # os.system('rar')
+                        # subprocess.call(['echo', 'Hello World!'])
+                    elif comicFormat == 'cbz':
+                        # comicZip.writestr(pageFN + '_-_' + str(i).zfill(3) + '.webp', pages[i].getvalue())
+                        pageName=pageFN + '_-_' + str(i).zfill(3) + '.webp'
+                        comicZip.writestr(pageName, pages[i].getvalue())
 
 
-# Split PDF into seperate pages
-if os.path.isdir(imgDir) == False:
-    os.mkdir(imgDir)
+                    # Update progress bar
+                    time.sleep(0.001)
+                    bar()
 
+        # Close archive file
+        comicZip.close()            
 
-for pdf in os.listdir(pdfDir):
-    baseFN=os.path.splitext(pdf)[0]
-    pageFN=baseFN.replace(' ', '_')
-    images = pdf2image.convert_from_path(pdfDir + '/' + pdf)
-    zip_file_bytes_io = io.BytesIO()
-    with ZipFile(zip_file_bytes_io, 'w') as zip_file:
-        with alive_bar(len(images), title='Trimming and archiving', length='20', bar='smooth', spinner='dots_reverse') as bar:
-            for i in range(len(images)):
-                # Trim pages
-                images[i] = trimBorder(images[i])
+        # Write archive to disk
+        # with open(outDir + '/' + baseFN + '.cbr', 'wb') as outFN:
+        comicZipName = str(outDir / baseFN) + '.cbr'
+        with open(comicZipName, 'wb') as outFN:
+            outFN.write(zipIO.getvalue())
+            outFN.close()
 
-                # Convert pages to ByteIO
-                file_object = io.BytesIO()
-                images[i].save(file_object, "WEBP")
-                images[i].close()
-                images[i] = file_object
-
-                # Add pages to archive
-                zip_file.writestr(pageFN + '_-_' + str(i).zfill(3) + '.webp', images[i].getvalue())
-
-                sleep(0.001)
-                bar()
-
-        zip_file.close()
-        
-
-# Write archive to disk
-with open(imgDir + '/' + baseFN + '.cbr', 'wb') as outFN:
-    outFN.write(zip_file_bytes_io.getvalue())
-    outFN.close()
+if __name__ == '__main__':
+   main(sys.argv[1:])
